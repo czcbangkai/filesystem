b22440f6b2e42a09cb24efa0e4b88d50fc658e8d
Jim Chen <czcbangkai@hotmail.com>
2017-05-02 23:56:29 -0400

    updated write

--- fslib.hpp
+++ fslib.hpp
@@ -11,21 +11,21 @@ extern Tokenizer 	g_filename_tokenizer;
 
 static int find_fat(unsigned short& start, int& offset);
 
-int 				f_open(const char *filename, int flags);
-size_t 				f_read(void *data, size_t size, int num, int fd);
-size_t 				f_write(void *data, size_t size, int num, int fd);
-int 				f_close(int fd);
-int 				f_seek(int offset, int whence, int fd);
-int 				f_rewind(int fd);
-int					f_stat(Stat *buf, int fd);
-int					f_remove(const char *filename);
-int				f_opendir(const char *filename);
-Stat	f_readdir(int dirfd);
-// int					f_closedir(int dirfd);
-// int					f_mkdir(const char *filename, int mode);
-// int					f_rmdir(const char *filename);
-// int					f_mount(const char *type, const char *dir, int flags, void *data);
-// int					f_umount(const char *dir, int flags);
+int 				f_open(Vnode *vn, const char *filename, int flags);
+size_t 				f_read(Vnode *vn, void *data, size_t size, int num, int fd);
+size_t 				f_write(Vnode *vn, void *data, size_t size, int num, int fd);
+int 				f_close(Vnode *vn, int fd);
+int 				f_seek(Vnode *vn, int offset, int whence, int fd);
+int 				f_rewind(Vnode *vn, int fd);
+int					f_stat(Vnode *vn, Stat *buf, int fd);
+// int					f_remove(Vnode *vn, const char *filename);
+// dir_t				f_opendir(Vnode *vn, const char *filename);
+// struct dirent_t*	f_readdir(Vnode *vn, dir_t* dirp);
+// int					f_closedir(Vnode *vn, dir_t* dirp);
+// int					f_mkdir(Vnode *vn, const char *filename, int mode);
+// int					f_rmdir(Vnode *vn, const char *filename);
+// int					f_mount(Vnode *vn, const char *type, const char *dir, int flags, void *data);
+// int					f_umount(Vnode *vn, const char *dir, int flags);
 
 
 
